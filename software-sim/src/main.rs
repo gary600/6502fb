8fec363808f040ee43d19f12340c3ae3444eb2cd
Carson Swoveland <sciencedude2003@gmail.com>
2023-11-17 21:48:14 -0500

    Run event loop on main thread to prevent crash

--- software-sim/src/main.rs
+++ software-sim/src/main.rs
@@ -1,6 +1,6 @@
 mod cpu;
 
-use std::{thread, sync::{mpsc::{channel, Receiver}, Mutex, Arc}, time::Duration, process::exit};
+use std::{thread, sync::{mpsc::{channel, Receiver, Sender}, Mutex, Arc}, time::Duration, process::exit};
 
 use cpu::{CpuContext, create_arch, CPU};
 
@@ -83,77 +83,83 @@ impl Default for Ctx {
     }
 }
 
-fn create_fb_thread(ctx: Arc<Mutex<Ctx>>) -> Receiver<()> {
-    let (tx_nmi, rx_nmi) = channel::<()>();
-    thread::spawn(move || {
-        let mut window = Window::new(
-            "6502 Framebuffer Emulator",
-            256, 256,
-            WindowOptions {
-                scale: Scale::X4,
-                ..Default::default()
-            }
-        ).expect("Could not create window");
-        
-        println!("Created window");
-        while window.is_open() {
-            let ctx = ctx.lock().unwrap();
-            // println!("frame");
-            if ctx.get_blank_black() {
-                window.update_with_buffer(&[0u32; 256*256], 256, 256).unwrap();
-            }
-            else if ctx.get_blank_white() {
-                window.update_with_buffer(&[0x00FFFFFFu32; 256*256], 256, 256).unwrap();
-            }
-            else {
-                window.update_with_buffer(
-                    &ctx.vram.map(|x| {
-                        let x = x as u32;
-                        ((x & 0b11100000) << 16) |
-                        ((x & 0b00011100) << (8 + 3)) |
-                        ((x & 0b00000011) << 6)
-                    }),
-                    256,
-                    256
-                ).unwrap();
-            }
-            let nmi = ctx.get_nmienable();
-            drop(ctx);
-            thread::sleep(Duration::from_micros(15253));
-            if nmi {tx_nmi.send(()).unwrap();}
-            thread::sleep(Duration::from_micros(1400));
+fn run_fb(ctx: Arc<Mutex<Ctx>>, tx_nmi: Sender<()>) -> Receiver<()> {
+    let mut window = Window::new(
+        "6502 Framebuffer Emulator",
+        256, 256,
+        WindowOptions {
+            scale: Scale::X4,
+            ..Default::default()
         }
+    ).expect("Could not create window");
+    
+    println!("Created window");
+    while window.is_open() {
+        let ctx = ctx.lock().unwrap();
+        // println!("frame");
+        if ctx.get_blank_black() {
+            window.update_with_buffer(&[0u32; 256*256], 256, 256).unwrap();
+        }
+        else if ctx.get_blank_white() {
+            window.update_with_buffer(&[0x00FFFFFFu32; 256*256], 256, 256).unwrap();
+        }
+        else {
+            window.update_with_buffer(
+                &ctx.vram.map(|x| {
+                    let x = x as u32;
+                    ((x & 0b11100000) << 16) |
+                    ((x & 0b00011100) << (8 + 3)) |
+                    ((x & 0b00000011) << 6)
+                }),
+                256,
+                256
+            ).unwrap();
+        }
+        let nmi = ctx.get_nmienable();
+        drop(ctx);
+        thread::sleep(Duration::from_micros(15253));
+        if nmi {tx_nmi.send(()).unwrap();}
+        thread::sleep(Duration::from_micros(1400));
+    }
 
-        println!("FB thread shutting down");
-        exit(0);
+    println!("FB thread shutting down");
+    exit(0);
+}
+
+fn create_cpu_thread(ctx: Arc<Mutex<Ctx>>, rx_nmi: Receiver<()>) {
+    thread::spawn(move || {
+        // eeprom[0x1FF0] = 0x4C;
+        let arch = create_arch::<Ctx>();
+
+        let mut cpu = CPU::new();
+
+        // let mut cycle = 0u32;
+        loop {
+            let mut ctx = ctx.lock().unwrap();
+            cpu.next(&mut *ctx, &arch);
+            if rx_nmi.try_recv().is_ok() {
+                cpu.trigger_nmi(&mut *ctx);
+                // println!("NMI!");
+            }
+            drop(ctx);
+
+            // if cycle % 1000 == 0 {println!("cycle");}
+            thread::sleep(Duration::from_micros(1));
+            // println!("cycle {cycle}");
+            // cycle += 1;
+        }
     });
-    rx_nmi
 }
 
 fn main() {
     let prog = std::fs::read(std::env::args().nth(1).unwrap()).unwrap();
     let mut eeprom = [0u8; 0x2000];
     eeprom.copy_from_slice(&prog[2..]);
-    // eeprom[0x1FF0] = 0x4C;
-    let arch = create_arch::<Ctx>();
     let ctx = Arc::new(Mutex::new(Ctx::new(eeprom)));
-    let mut cpu = CPU::new();
 
-    let rx_nmi = create_fb_thread(ctx.clone());
+    let (tx_nmi, rx_nmi) = channel::<()>();
 
-    // let mut cycle = 0u32;
-    loop {
-        let mut ctx = ctx.lock().unwrap();
-        cpu.next(&mut *ctx, &arch);
-        if rx_nmi.try_recv().is_ok() {
-            cpu.trigger_nmi(&mut *ctx);
-            // println!("NMI!");
-        }
-        drop(ctx);
+    create_cpu_thread(ctx.clone(), rx_nmi);
 
-        // if cycle % 1000 == 0 {println!("cycle");}
-        thread::sleep(Duration::from_micros(1));
-        // println!("cycle {cycle}");
-        // cycle += 1;
-    }
+    run_fb(ctx, tx_nmi);
 }
